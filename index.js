8d3bed36fdac3f8cc57bb13a017da1a14073ceac
Aadish Bansal <aadibansal901@gmail.com>
2022-07-25 20:13:26 +0530

    Function added

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 function abc() {
-  const password = document.getElementById("exampleInputPassword1").value;
   const uname = document.getElementById("floatingInput").value;
   const email = document.getElementById("exampleInputEmail1").value;
-  const consent = document.getElementById("exampleCheck1").value;
-  if (uname.length == 0 || email.length == 0 || password.length == 0) {
-    alert("Feilds can't be empty");
-    return;
+  const password = document.getElementById("exampleInputPassword1").value;
+  const consent = document.getElementById("exampleCheck1");
+  let success = true;
+  console.log(password);
+  if (uname.length < 3) {
+    document.getElementById("validateExampleInput").innerHTML =
+      " User name can't be less than 3 characters";
+    success = false;
   }
-  if(uname.length >3){
-    alert("")   
+  if (!email.includes("@") || !email.includes(".com")) {
+    document.getElementById("validateExampleEmail").innerHTML =
+      "Invalid email address";
+    //   console.log("invalid email address");
+    success = false;
+  }
+  if (password.length < 7) {
+    document.getElementById("validateExamplePassword").innerHTML =
+      "Please provide a stronger password";
+    success = false;
+  }
+
+  if (!consent.checked) {
+    document.getElementById("validateExampleConsent").innerHTML =
+      "Please accept the terms and conditions";
+    // console.log("please accept the terms and conditions");
+    success = false;
+  }
+  if (success) {
+    alert("Regeistered successfully!");
+    document.getElementById("validateExampleInput").innerHTML = "";
+    document.getElementById("validateExampleEmail").innerHTML = "";
+    document.getElementById("validateExamplePassword").innerHTML = "";
+    document.getElementById("validateExampleConsent").innerHTML = "";
+  } else {
+    alert("Regeistered failed!");
   }
 }
