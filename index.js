2287c1b9dc4f75dfe6decf5ccf412bd31855bc4b
Aadish Bansal <aadibansal901@gmail.com>
2022-07-27 16:05:23 +0530

    styles added

--- index.js
+++ index.js
@@ -1,40 +1,51 @@
-function abc() {
-  const uname = document.getElementById("floatingInput").value;
-  const email = document.getElementById("exampleInputEmail1").value;
-  const password = document.getElementById("exampleInputPassword1").value;
-  const consent = document.getElementById("exampleCheck1");
+function validate() {
+  const uname = document.getElementById("floatingInputName").value;
+  const email = document.getElementById("floatingInputEmail").value;
+  const consent = document.getElementById("exampleCheck");
+  const password = document.getElementById("floatingPassword").value;
+
   let success = true;
-  console.log(password);
   if (uname.length < 3) {
-    document.getElementById("validateExampleInput").innerHTML =
+    document.getElementById("validateInput").innerHTML =
       " User name can't be less than 3 characters";
     success = false;
   }
   if (!email.includes("@") || !email.includes(".com")) {
-    document.getElementById("validateExampleEmail").innerHTML =
+    document.getElementById("validateEmail").innerHTML =
       "Invalid email address";
     //   console.log("invalid email address");
     success = false;
   }
   if (password.length < 7) {
-    document.getElementById("validateExamplePassword").innerHTML =
+    document.getElementById("validatePassword").innerHTML =
       "Please provide a stronger password";
     success = false;
   }
 
   if (!consent.checked) {
-    document.getElementById("validateExampleConsent").innerHTML =
+    document.getElementById("validateConsent").innerHTML =
       "Please accept the terms and conditions";
     // console.log("please accept the terms and conditions");
     success = false;
   }
   if (success) {
+    console.log(success);
+    document.getElementById("validateInput").innerHTML = "";
+    document.getElementById("validateEmail").innerHTML = "";
+    document.getElementById("validatePassword").innerHTML = "";
+    document.getElementById("validateConsent").innerHTML = "";
     alert("Regeistered successfully!");
-    document.getElementById("validateExampleInput").innerHTML = "";
-    document.getElementById("validateExampleEmail").innerHTML = "";
-    document.getElementById("validateExamplePassword").innerHTML = "";
-    document.getElementById("validateExampleConsent").innerHTML = "";
   } else {
     alert("Regeistered failed!");
   }
+}
+function passwordCheck() {
+  const password = document.getElementById("floatingPassword").value;
+  if (password.length < 7) {
+    document.getElementById("validatePassword").innerHTML =
+      "Password should be have atleast 8 characters";
+  } else {
+    document.getElementById("validatePassword").innerHTML = "";
+  }
+  console.log(password);
 }
